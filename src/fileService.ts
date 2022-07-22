23e1871fe52fe74375f2fd2aaed048abc7c99e9c
K@mi1 <kamil.durhan.90@gmail.com>
2022-07-22 18:54:29 +0200

    code formatted with prettier

--- src/fileService.ts
+++ src/fileService.ts
@@ -1,79 +1,72 @@
-import path from "path";
-import {existsSync, promises as fs} from "fs";
-import imageResize from "./imageProcessing";
+import path from 'path'
+import { existsSync, promises as fs } from 'fs'
+import imageResize from './imageProcessing'
 
-interface Image{
-    filename?:string,
-    width?:string,
-    height?:string;
+interface Image {
+    filename?: string
+    width?: string
+    height?: string
 }
 
 export default class File {
-    static filepath = path.resolve("./images/");  
-    static thumbPath = path.resolve("./images/thumbs/");
+    static filepath = path.resolve('./images/')
+    static thumbPath = path.resolve('./images/thumbs/')
 
     //getting full filenames from folder
     static getAvailableNamesFromFolder = async () => {
-        const readDirectory = await fs.readdir(File.filepath);
-        const name = readDirectory.map((filename) => filename.split(".")[0]);
-        
-        return name;
+        const readDirectory = await fs.readdir(File.filepath)
+        const name = readDirectory.map((filename) => filename.split('.')[0])
+
+        return name
     }
-    static async validator(filename: string):Promise<boolean> {
-        const pathLike = path.resolve(this.filepath, `${filename}.jpg`);
-        if(await existsSync(pathLike))
-        {
-            return true;
-        }
-        else return false;
+    static async validator(filename: string): Promise<boolean> {
+        const pathLike = path.resolve(this.filepath, `${filename}.jpg`)
+        if (await existsSync(pathLike)) {
+            return true
+        } else return false
     }
-    static getImage =async (reqFilename: Image) :Promise<void|string|null> =>{
-        if(reqFilename.filename !== undefined)
-        {
-            const validation = await this.validator(reqFilename.filename)
-            console.log(`Validation of Filename is: ${await this.validator(reqFilename.filename)}`)
-            if(validation){
+    static getImage = async (reqFile: Image): Promise<void | string | null> => {
+        if (reqFile.filename !== undefined) {
+            const validation = await this.validator(reqFile.filename)
+            console.log(`Validation of Filename is: ${await this.validator(reqFile.filename)}`)
+            if (validation) {
                 //if user query is needed to sharp
-                if(reqFilename.width !== undefined && reqFilename.height !== undefined)   
-                {
-                    try{
+                if (reqFile.width !== undefined && reqFile.height !== undefined) {
+                    try {
                         //trying to acccess the thumb folder. If we can access we don't need to create it.
                         await fs.access(this.thumbPath)
                         //Resizing the image after we can access the thumb folder and save the image there
                         await imageResize({
-    
-                            source: `${ this.filepath}\\${ reqFilename.filename}.jpg`,
-                            target: `${ this.thumbPath}\\${reqFilename.filename}-${ reqFilename.height}x${ reqFilename.width}.jpg`,
-                            width: parseInt(reqFilename.width),
-                            height: parseInt(reqFilename.height)
+                            source: `${this.filepath}\\${reqFile.filename}.jpg`,
+                            target: `${this.thumbPath}\\${reqFile.filename}-${reqFile.height}x${reqFile.width}.jpg`,
+                            width: parseInt(reqFile.width),
+                            height: parseInt(reqFile.height),
                         })
                         //call the requested image
-                       return `${ this.thumbPath}\\${reqFilename.filename}-${ reqFilename.height}x${ reqFilename.width}.jpg`
-                    }
-                    catch{
+                        return `${this.thumbPath}\\${reqFile.filename}-${reqFile.height}x${reqFile.width}.jpg`
+                    } catch {
                         //if there is no thumb folder here will it created
                         fs.mkdir(this.thumbPath)
                         //Resizing the image after we can access the thumb folder and save the image there
                         await imageResize({
-                            source: `${ this.filepath}\\${ reqFilename.filename}.jpg`,
-                            target: `${ this.thumbPath}\\${reqFilename.filename}-${ reqFilename.height}x${ reqFilename.width}.jpg`,
-                            width: parseInt(reqFilename.width),
-                            height: parseInt(reqFilename.height)
+                            source: `${this.filepath}\\${reqFile.filename}.jpg`,
+                            target: `${this.thumbPath}\\${reqFile.filename}-${reqFile.height}x${reqFile.width}.jpg`,
+                            width: parseInt(reqFile.width),
+                            height: parseInt(reqFile.height),
                         })
                         //call the requested image
-                        return `${ this.thumbPath}\\${reqFilename.filename}-${ reqFilename.height}x${ reqFilename.width}.jpg`
+                        return `${this.thumbPath}\\${reqFile.filename}-${reqFile.height}x${reqFile.width}.jpg`
                     }
                     //if there is no width and height we just need to validate the requested Image Filename and response it.
-                }else{
-                    reqFilename.width = undefined;
-                    reqFilename.height=undefined;
+                } else {
+                    reqFile.width = undefined
+                    reqFile.height = undefined
                     //returning images path with requested image
-                    return `${ this.filepath}\\${reqFilename.filename}.jpg`
+                    return `${this.filepath}\\${reqFile.filename}.jpg`
                 }
             }
-
         }
         //return null if validation fails so the error page will be responsed
-        else return null;
+        else return null
     }
 }
--- src/routes/image.ts
+++ src/routes/image.ts
@@ -1,31 +1,32 @@
-import { Router } from "express";
-import express from "express";
-import {createReadStream} from "fs";
-import File from "../fileService";
+import { Router } from 'express'
+import express from 'express'
+import { createReadStream } from 'fs'
+import File from '../fileService'
 
-const contentType = "image/jpeg";
-const images: Router = express.Router();
+const contentType = 'image/jpeg'
+const images: Router = express.Router()
 
-images.get("/", async (req: express.Request, res: express.Response) => {
+images.get('/', async (req: express.Request, res: express.Response) => {
     {
-           const data: string| null |void = await File.getImage(req.query);
+        const data: string | null | void = await File.getImage(req.query)
         //if data is not null so the image will be in the images folder so we can handle it
-        if(data !== null && data !== undefined){
-                 const stream = createReadStream(await data)
-                 return stream.on("open", function() {
-                    res.set("Content-Type", contentType);
-                    stream.pipe(res);
-                })
+        if (data !== null && data !== undefined) {
+            const stream = createReadStream(await data)
+            return stream.on('open', function () {
+                res.set('Content-Type', contentType)
+                stream.pipe(res)
+            })
         }
         //there will data be null. So the user has given a wrong query
-        else{
+        else {
             res.send(`please choose from the filenames below and specify it as shown in the example <br>
-       ${(await File.getAvailableNamesFromFolder()).splice(1,4,"")}<br>
+       ${(await File.getAvailableNamesFromFolder()).splice(1, 4, '')}<br>
        or use the right query segments like this: <br>
        <a href="/images?filename=icelandwaterfall"> localhost:3000/images?filename=icelandwaterfall.jpg</a>
        <br>
        localhost:3000/images?filename=<i><b>(here you need to give the correct filename which is above provided)</i>`)
         }
-    }});
+    }
+})
 
-export default images;
+export default images
